c6168f5ed86581598cedc0774c8065231ec9ee29
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-04-25 23:52:46 +0200

    add postgreSQL database to store user information and password

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const cors = require('cors')
+const knex = require('knex')
+
+const db = knex ({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        // port: 3306,
+        user: 'postgres',
+        password: 'root',
+        database: 'smart-brain',
+    },
+});
+
+// db.select('*').from('users').then(data => {
+//     console.log(data)
+// }) 
+    // Starts an asynchronous operation (it talks to the database).
+    // It doesn't immediately give you the data — instead, it returns a Promise.
+    // use .then(...) to handle the result when the Promise is fulfilled.
+
 
 const app = express()
 
@@ -53,12 +73,19 @@ app.post('/signin', (req,res) => {
     // bcrypt.compare('veggies', '$2b$10$T91v8OQ1AJcRKYV3QzUVoOc0WB5Q5qgUd9S/2xJ.zuI11pPxiU.N.', (err, res) => {
     //     console.log('second', res)   
     // });
+
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-            res.json('success')
-    } else {
-        res.status(400).json('error logging in')
-    } 
+            // res.json('success')
+            res.json(database.users[0])
+        } else {
+            res.status(400).json('error logging in')
+        } 
+
+
+    
+
+
 })
 
 app.post('/register', (req,res) => {
@@ -66,30 +93,55 @@ app.post('/register', (req,res) => {
     // bcrypt.hash(password, 10, (err, hash) => {
     //     console.log(hash)   
     // });
-    database.users.push({
-        id: '125',
-        name: name,
+
+    // database.users.push({
+    //     id: '125',
+    //     name: name,
+    //     email: email,
+    //     // password: password,
+    //     entries: 0,
+    //     joined: new Date()
+    // })
+    // res.json(database.users[database.users.length-1])
+
+    db('users')
+    .returning('*')
+    .insert({
         email: email,
-        // password: password,
-        entries: 0,
+        name: name,
         joined: new Date()
+    }).then(response => {
+        res.json(response);
     })
-    res.json(database.users[database.users.length-1])
+    .catch(err => res.status(404).json('unable to register'))
+        // res is the response object from Express
+        // .json() sends the err back to the client in JSON format
+ 
     
 })
 
 app.get('/profile/:id', (req,res) => {
     const {id} = req.params
     let found = false
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true
-            return res.json(user)
-        } 
-    }) 
-    if (!found) {
-        res.status(404).json('no such user')
-    }
+    
+    // database.users.forEach(user => {
+    //     if (user.id === id) {
+    //         found = true
+    //         return res.json(user)
+    //     } 
+    // }) 
+
+    // db.select('*').from('users').where({
+    //     id: id
+    // }) //equal
+    db.select('*').from('users').where({id})
+    .then(user => {
+        // console.log(user[0])
+        res.json(user[0])
+    })
+    // if (!found) {
+    //     res.status(404).json('no such user')
+    // }
         
 })
 
